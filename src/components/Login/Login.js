378148f754d685b16795408d5868c3d9fb4c80ed
Manwendra-Chauhan <manwendrachauhan1311@gmail.com>
2023-11-07 22:26:09 +0530

    made changes in login section

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,20 +3,18 @@ import styles from "./Login.module.css";
 const Login = () => {
   return (
     <div className={styles.body}>
-      <div className={styles.container}>
-        <p className={styles.header}>Inventory Management System</p>
-        <form action="" className={styles.login_body}>
-          <div class={styles.login_inputs_container}>
-            <label for="">Username</label>
-            <input placeholder="Username" type="text" />
-          </div>
-          <div className={styles.login_inputs_container}>
-            <label for="">Password</label>
-            <input placeholder="Password" type="password" />
-          </div>
-          <button className={styles.login_button}>login</button>
-        </form>
-      </div>
+      <form action="" className={styles.login_body}>
+        <span className={styles.header}>Login</span>
+        <div class={styles.login_inputs_container}>
+          <label for="">Username</label>
+          <input placeholder="Username" type="text" />
+        </div>
+        <div className={styles.login_inputs_container}>
+          <label for="">Password</label>
+          <input placeholder="Password" type="password" />
+        </div>
+        <button className={styles.login_button}>login</button>
+      </form>
     </div>
   );
 };
